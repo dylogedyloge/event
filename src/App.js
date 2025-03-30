5220924cde1c0edee91fc900e4065075e7f16ca8
amir <dyloge@gmail.com>
2025-03-30 13:02:01 +0330

    remove unused imports and simplify App component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Event from './Event';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Event/>
     </div>
   );
