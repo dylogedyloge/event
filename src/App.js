d87348cf0e8def91d0d459eed9a8f59c002bbae3
amir <dyloge@gmail.com>
2025-04-03 14:32:49 +0330

    remove white border around the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import Event from './Event';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
     <div >
+      <CssBaseline />
       <Event/>
     </div>
   );
